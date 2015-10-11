b34f66a9fff83d6bd9bd401bf80f7a059045ba6d
René Olivo <minet.ws@gmail.com>
2015-10-11 15:57:19 -0400

    add color range controllers

--- src/widgets/range-controller.js
+++ src/widgets/range-controller.js
@@ -7,10 +7,16 @@ import {ObserverLocator} from 'aurelia-binding';
 export class RangeController {
   @bindable width;
   @bindable height;
+  @bindable red;
+  @bindable green;
+  @bindable blue;
 
   constructor(element, noUiSlider, ObserverLocator) {
     this.width = 100;
     this.height = 100;
+    this.red = 125;
+    this.green = 125;
+    this.blue = 125;
 
     setTimeout(()=> this.width = 400, 5000);
 
@@ -20,22 +26,36 @@ export class RangeController {
   }
 
   created() {
-    var widthRange = this.element.querySelector('.width');
-    var heightRange = this.element.querySelector('.height');
+    var widthRange = this.element.querySelector('.range.width');
+    var heightRange = this.element.querySelector('.range.height');
+
+    var redRange = this.element.querySelector('.range.red');
+    var greenRange = this.element.querySelector('.range.green');
+    var blueRange = this.element.querySelector('.range.blue');
 
     this.createRange(widthRange, 'width');
     this.createRange(heightRange, 'height');
+
+    var colorRangeConfig = {orientation: 'vertical', range: {min: 0, max: 255}};
+
+    this.createRange(redRange, 'red', colorRangeConfig);
+    this.createRange(greenRange, 'green', colorRangeConfig);
+    this.createRange(blueRange, 'blue', colorRangeConfig);
   }
 
-  createRange(element, propName) {
-    this.noUiSlider.create(element, {
+  createRange(element, propName, config = {}) {
+    var defaultConfig = {
       start: this[propName],
       step: 5,
       range: {
         min: 0,
         max: 500
       }
-    });
+    };
+
+    config = Object.assign(defaultConfig, config);
+
+    this.noUiSlider.create(element, config);
 
     element.noUiSlider.on('slide', (value) => {
       this[propName] = parseInt(value);
