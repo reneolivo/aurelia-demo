3eae0956150b3b01d58616a78a43db0733b84541
René Olivo <minet.ws@gmail.com>
2015-10-11 20:05:56 -0400

    move methods to modal class

--- src/widgets/modal.js
+++ src/widgets/modal.js
@@ -1,6 +1,20 @@
-import {customElement} from 'aurelia-framework';
+import {inject, customElement} from 'aurelia-framework';
+import $ from 'jquery';
 
 @customElement('modal')
+@inject(Element)
 export class Modal {
-  
+  modal;
+
+  constructor(element) {
+    this.element = element;
+  }
+
+  created() {
+    this.modal = $(this.element).find('.modal');
+  }
+
+  open() {
+    this.modal.modal('show');
+  }
 }
