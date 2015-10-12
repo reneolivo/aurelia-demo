6b6cebf8bd4b3c8f813ebaf9f70d5177532aed04
René Olivo <minet.ws@gmail.com>
2015-10-12 00:20:58 -0400

    initial gallery structure

--- src/gallery.js
+++ src/gallery.js
@@ -1,3 +1,19 @@
+import {inject} from 'aurelia-framework';
+import {HttpClient} from 'aurelia-http-client';
+
+@inject(HttpClient)
 export class Gallery {
   images = [];
+
+  constructor(http) {
+    this.http = http;
+
+    this.fetchImages();
+  }
+
+  fetchImages() {
+    this.http.get('/data/images.json').then(response => {
+      this.images = response.content;
+    });
+  }
 }
